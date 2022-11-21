cc3a5e4f4718a5edf2202429ecdd3acf233a7ccf
Thomas Sicking <thomas.sicking@opernikus.de>
2022-11-21 18:29:16 +0100

    AcThor merged from working repository

---
+++ io.openems.edge.io.acthor/src/io/openems/edge/io/acthor/AcThor.java
@@ -0,0 +1,173 @@
+package io.openems.edge.io.acthor;
+
+import java.util.Optional;
+
+import io.openems.common.channel.AccessMode;
+import io.openems.common.channel.PersistencePriority;
+import io.openems.common.channel.Unit;
+import io.openems.common.exceptions.OpenemsError.OpenemsNamedException;
+import io.openems.common.types.OpenemsType;
+import io.openems.edge.common.channel.BooleanDoc;
+import io.openems.edge.common.channel.BooleanWriteChannel;
+import io.openems.edge.common.channel.Doc;
+import io.openems.edge.common.channel.IntegerWriteChannel;
+import io.openems.edge.common.component.OpenemsComponent;
+
+public interface AcThor extends OpenemsComponent {
+
+	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
+		POWER(Doc.of(OpenemsType.INTEGER).unit(Unit.WATT).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.HIGH).text("Power")),
+		TEMP_1(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Temp 1")),
+		H_W_1_MAX(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Hot Water 1 max")),
+		STATUS(Doc.of(OpenemsType.INTEGER).unit(Unit.NONE).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Status")),
+		POWER_TIMEOUT(Doc.of(OpenemsType.INTEGER).unit(Unit.SECONDS).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Power Timeout")),
+		BOOST_MODE(Doc.of(OpenemsType.INTEGER).unit(Unit.NONE).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Boost Mode")),
+		H_W_1_MIN(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Hot Water 1 min")),
+		BOOST_TIME_1_START(Doc.of(OpenemsType.INTEGER).unit(Unit.NONE).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Boost time 1 start")),
+		BOOST_TIME_1_STOP(Doc.of(OpenemsType.INTEGER).unit(Unit.NONE).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Boost time 1 stop")),
+		BOOST_ACTIVATE(Doc.of(OpenemsType.BOOLEAN).unit(Unit.NONE).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Boost activate")),
+		AC_THOR_NUMBER(Doc.of(OpenemsType.INTEGER).unit(Unit.NONE).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("AC Thor number")),
+		MAX_POWER(Doc.of(OpenemsType.INTEGER).unit(Unit.WATT).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("max Power")),
+		TEMPCHIP(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("tempchip")),
+		CONTROL_FIRMWARE_VERSION(Doc.of(OpenemsType.INTEGER).unit(Unit.NONE).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Control Firmware version")),
+		PS_FIRMWARE_VERSION(Doc.of(OpenemsType.INTEGER).unit(Unit.NONE).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("PS Firmware version")),
+		SERIAL_NUMBER(Doc.of(OpenemsType.STRING).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Serial number")),
+		BOOST_TIME_2_START(Doc.of(OpenemsType.INTEGER).unit(Unit.NONE).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Boost time 2 start")),
+		BOOST_TIME_2_STOP(Doc.of(OpenemsType.INTEGER).unit(Unit.NONE).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Boost time 2 stop")),
+		CONTROL_FIRMWARE_SUB_VERSION(Doc.of(OpenemsType.INTEGER).unit(Unit.NONE).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Control Firmware sub version")),
+		TEMP_2(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Temp 2")),
+		TEMP_3(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Temp 3")),
+		TEMP_4(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Temp 4")),
+		H_W_2_MAX(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Hot Water 2 max")),
+		H_W_3_MAX(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Hot Water 3 max")),
+		H_W_2_MIN(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Hot Water 2 min")),
+		H_W_3_MIN(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Hot Water 3 min")),
+		RH_1_MAX(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Room heating 1 max")),
+		RH_2_MAX(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Room heating 2 max")),
+		RH_3_MAX(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Room heating 3 max")),
+		RH_1_DAY_MIN(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Room heating 1 day min")),
+		RH_2_DAY_MIN(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Room heating 2 day min")),
+		RH_3_DAY_MIN(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Room heating 3 day min")),
+		RH_1_NIGHT_MIN(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Room heating 1 night min")),
+		RH_2_NIGHT_MIN(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Room heating 2 night min")),
+		RH_3_NIGHT_MIN(Doc.of(OpenemsType.INTEGER).unit(Unit.DEZIDEGREE_CELSIUS).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Room heating 3 night min")),
+		NIGHT_FLAG(Doc.of(OpenemsType.BOOLEAN).unit(Unit.NONE).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Night flag")),
+		STRATIFACTION_FLAG(Doc.of(OpenemsType.BOOLEAN).unit(Unit.NONE).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Stratifaction flag")),
+		RELAY_1_STATUS(Doc.of(OpenemsType.BOOLEAN).unit(Unit.ON_OFF).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Relay 1 status")),
+		LOAD_NOMINAL_POWER(Doc.of(OpenemsType.INTEGER).unit(Unit.WATT).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("load nominal power")),
+		VOLTAGE_OUT(Doc.of(OpenemsType.INTEGER).unit(Unit.VOLT).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Voltage out")),
+		METER_POWER(Doc.of(OpenemsType.INTEGER).unit(Unit.WATT).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Meter Power")),
+		PMAX_ABS(Doc.of(OpenemsType.INTEGER).unit(Unit.WATT).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Max power currently possible")),
+		P_OUT_1(Doc.of(OpenemsType.INTEGER).unit(Unit.WATT).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("P out 1")),
+		P_OUT_2(Doc.of(OpenemsType.INTEGER).unit(Unit.WATT).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("P out 2")),
+		P_OUT_3(Doc.of(OpenemsType.INTEGER).unit(Unit.WATT).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("P out 3")),
+		OPERATION_STATE(Doc.of(OpenemsType.INTEGER).unit(Unit.NONE).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Operation state")),
+		DEVICE_STATE(Doc.of(OpenemsType.INTEGER).unit(Unit.NONE).accessMode(AccessMode.READ_WRITE)
+				.persistencePriority(PersistencePriority.LOW).text("Device state")),
+		POWER_OF_QUERIED_DEVICE(Doc.of(OpenemsType.INTEGER).unit(Unit.WATT).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Power of the queried device")),
+		SOLAR_PART_OF_DEVICE_POWER(Doc.of(OpenemsType.INTEGER).unit(Unit.WATT).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Solar part of device power")),
+		GRID_PART_OF_DEVICE_POWER(Doc.of(OpenemsType.INTEGER).unit(Unit.WATT).accessMode(AccessMode.READ_ONLY)
+				.persistencePriority(PersistencePriority.LOW).text("Grid part of device power")),
+
+		// Dummy channels to make it IO
+		OUT_1(new BooleanDoc().accessMode(AccessMode.READ_WRITE)),
+		OUT_2(new BooleanDoc().accessMode(AccessMode.READ_WRITE)),
+		OUT_3(new BooleanDoc().accessMode(AccessMode.READ_WRITE)),;
+
+		private final Doc doc;
+
+		private ChannelId(Doc doc) {
+			this.doc = doc;
+		}
+
+		@Override
+		public Doc doc() {
+			return this.doc;
+		}
+	}
+
+	public default IntegerWriteChannel getPowerWriteChannel() {
+		return this.channel(ChannelId.POWER);
+	}
+
+	public default Optional<Integer> getWritePower() {
+		return this.getPowerWriteChannel().value().asOptional();
+	}
+
+	public default void setWritePower(Integer value) throws OpenemsNamedException {
+		this.getPowerWriteChannel().setNextWriteValue(value);
+	}
+
+	public default BooleanWriteChannel getOut1WriteChannel() {
+		return this.channel(ChannelId.OUT_1);
+	}
+
+	public default Optional<Boolean> getOut1() {
+		return this.getOut1WriteChannel().value().asOptional();
+	}
+
+	public default BooleanWriteChannel getOut2WriteChannel() {
+		return this.channel(ChannelId.OUT_2);
+	}
+
+	public default Optional<Boolean> getOut2() {
+		return this.getOut2WriteChannel().value().asOptional();
+	}
+
+	public default BooleanWriteChannel getOut3WriteChannel() {
+		return this.channel(ChannelId.OUT_3);
+	}
+
+	public default Optional<Boolean> getOut3() {
+		return this.getOut3WriteChannel().value().asOptional();
+	}
+
+}
---
+++ io.openems.edge.io.acthor/src/io/openems/edge/io/acthor/AcThorImpl.java
@@ -0,0 +1,246 @@
+package io.openems.edge.io.acthor;
+
+import java.util.Optional;
+
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventHandler;
+import org.osgi.service.event.propertytypes.EventTopics;
+import org.osgi.service.metatype.annotations.Designate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.openems.common.exceptions.OpenemsError.OpenemsNamedException;
+import io.openems.common.exceptions.OpenemsException;
+import io.openems.edge.bridge.modbus.api.AbstractOpenemsModbusComponent;
+import io.openems.edge.bridge.modbus.api.BridgeModbus;
+import io.openems.edge.bridge.modbus.api.ElementToChannelConverter;
+import io.openems.edge.bridge.modbus.api.ModbusComponent;
+import io.openems.edge.bridge.modbus.api.ModbusProtocol;
+import io.openems.edge.bridge.modbus.api.element.DummyRegisterElement;
+import io.openems.edge.bridge.modbus.api.element.SignedWordElement;
+import io.openems.edge.bridge.modbus.api.element.StringWordElement;
+import io.openems.edge.bridge.modbus.api.element.UnsignedWordElement;
+import io.openems.edge.bridge.modbus.api.task.FC3ReadRegistersTask;
+import io.openems.edge.bridge.modbus.api.task.FC6WriteRegisterTask;
+import io.openems.edge.common.channel.BooleanWriteChannel;
+import io.openems.edge.common.component.OpenemsComponent;
+import io.openems.edge.common.event.EdgeEventConstants;
+import io.openems.edge.common.taskmanager.Priority;
+import io.openems.edge.io.api.DigitalOutput;
+import io.openems.edge.meter.api.AsymmetricMeter;
+import io.openems.edge.meter.api.MeterType;
+import io.openems.edge.meter.api.SymmetricMeter;
+import io.openems.edge.timedata.api.Timedata;
+import io.openems.edge.timedata.api.TimedataProvider;
+import io.openems.edge.timedata.api.utils.CalculateEnergyFromPower;
+
+@Designate(ocd = Config.class, factory = true)
+@Component(//
+		name = "Heater.Ac.Thor", //
+		immediate = true, //
+		configurationPolicy = ConfigurationPolicy.REQUIRE //
+)
+@EventTopics({ //
+		EdgeEventConstants.TOPIC_CYCLE_BEFORE_PROCESS_IMAGE, //
+		EdgeEventConstants.TOPIC_CYCLE_AFTER_PROCESS_IMAGE //
+})
+public class AcThorImpl extends AbstractOpenemsModbusComponent implements AcThor, EventHandler, ModbusComponent,
+		SymmetricMeter, AsymmetricMeter, DigitalOutput, TimedataProvider, OpenemsComponent {
+
+	private Config config = null;
+
+	private final Logger log = LoggerFactory.getLogger(AcThorImpl.class);
+
+	private final BooleanWriteChannel[] outputChannels;
+
+	public AcThorImpl() {
+		super(//
+				OpenemsComponent.ChannelId.values(), //
+				ModbusComponent.ChannelId.values(), //
+				SymmetricMeter.ChannelId.values(), //
+				AsymmetricMeter.ChannelId.values(), //
+				AcThor.ChannelId.values() //
+		);
+		this.outputChannels = new BooleanWriteChannel[] { this.channel(AcThor.ChannelId.OUT_1),
+				this.channel(AcThor.ChannelId.OUT_2), this.channel(AcThor.ChannelId.OUT_3) };
+	}
+
+	@Reference
+	protected ConfigurationAdmin cm;
+
+	@Reference(policy = ReferencePolicy.STATIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.MANDATORY)
+	protected void setModbus(BridgeModbus modbus) {
+		super.setModbus(modbus);
+	}
+
+	@Reference(policy = ReferencePolicy.DYNAMIC, policyOption = ReferencePolicyOption.GREEDY, cardinality = ReferenceCardinality.OPTIONAL)
+	private volatile Timedata timedata = null;
+
+	private final CalculateEnergyFromPower calculateConsumptionEnergy = new CalculateEnergyFromPower(this,
+			SymmetricMeter.ChannelId.ACTIVE_CONSUMPTION_ENERGY);
+
+	@Activate
+	void activate(ComponentContext context, Config config) throws OpenemsException {
+		this.config = config;
+		if (super.activate(context, config.id(), config.alias(), config.enabled(), config.modbusUnitId(), this.cm,
+				"Modbus", config.modbus_id())) {
+			return;
+		}
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		super.deactivate();
+	}
+
+	@Override
+	public void handleEvent(Event event) {
+		if (!this.isEnabled()) {
+			return;
+		}
+		switch (event.getTopic()) {
+		case EdgeEventConstants.TOPIC_CYCLE_BEFORE_PROCESS_IMAGE:
+			try {
+				Integer calculatedPower = this.calculatePower();
+				this.setWritePower(calculatedPower);
+			} catch (OpenemsNamedException e) {
+				this.logError(this.log, "Unable to set power ex:" + e.getMessage());
+			}
+			break;
+		case EdgeEventConstants.TOPIC_CYCLE_AFTER_PROCESS_IMAGE:
+			this.calculateConsumptionEnergy.update(this.getActivePower().orElse(0));
+			break;
+		}
+	}
+
+	private Integer calculatePower() {
+		Optional<Boolean> out1 = this.getOut1WriteChannel().getNextWriteValue(); // TODO Use getNextWriteValueAndReset
+		Optional<Boolean> out2 = this.getOut2WriteChannel().getNextWriteValue();
+		Optional<Boolean> out3 = this.getOut3WriteChannel().getNextWriteValue();
+		Integer power = 0;
+		if (out1.orElse(false)) {
+			power += config.powerStep();
+		}
+		if (out2.orElse(false)) {
+			power += config.powerStep();
+		}
+		if (out3.orElse(false)) {
+			power += config.powerStep();
+		}
+		return power;
+	}
+
+	@Override
+	protected ModbusProtocol defineModbusProtocol() throws OpenemsException {
+		return new ModbusProtocol(this,
+				new FC3ReadRegistersTask(1000, Priority.HIGH, m(AcThor.ChannelId.POWER, new UnsignedWordElement(1000)),
+						m(AcThor.ChannelId.TEMP_1, new UnsignedWordElement(1001)),
+						m(AcThor.ChannelId.H_W_1_MAX, new UnsignedWordElement(1002)),
+						m(AcThor.ChannelId.STATUS, new UnsignedWordElement(1003)),
+						m(AcThor.ChannelId.POWER_TIMEOUT, new UnsignedWordElement(1004)),
+						m(AcThor.ChannelId.BOOST_MODE, new UnsignedWordElement(1005)),
+						m(AcThor.ChannelId.H_W_1_MIN, new UnsignedWordElement(1006)),
+						m(AcThor.ChannelId.BOOST_TIME_1_START, new UnsignedWordElement(1007)),
+						m(AcThor.ChannelId.BOOST_TIME_1_STOP, new UnsignedWordElement(1008))),
+				// 1009-1011 Hour, minute, second
+
+				new FC3ReadRegistersTask(1012, Priority.LOW,
+						m(AcThor.ChannelId.BOOST_ACTIVATE, new UnsignedWordElement(1012)),
+						m(AcThor.ChannelId.AC_THOR_NUMBER, new UnsignedWordElement(1013)),
+						m(AcThor.ChannelId.MAX_POWER, new UnsignedWordElement(1014)),
+						m(AcThor.ChannelId.TEMPCHIP, new UnsignedWordElement(1015)),
+						m(AcThor.ChannelId.CONTROL_FIRMWARE_VERSION, new UnsignedWordElement(1016)),
+						m(AcThor.ChannelId.PS_FIRMWARE_VERSION, new UnsignedWordElement(1017)),
+						m(AcThor.ChannelId.SERIAL_NUMBER, new StringWordElement(1018, 8)),
+						m(AcThor.ChannelId.BOOST_TIME_2_START, new UnsignedWordElement(1026)),
+						m(AcThor.ChannelId.BOOST_TIME_2_STOP, new UnsignedWordElement(1027)),
+						m(AcThor.ChannelId.CONTROL_FIRMWARE_SUB_VERSION, new UnsignedWordElement(1028)),
+						new DummyRegisterElement(1029), // control firmware update available
+						m(AcThor.ChannelId.TEMP_2, new UnsignedWordElement(1030)),
+						m(AcThor.ChannelId.TEMP_3, new UnsignedWordElement(1031)),
+						m(AcThor.ChannelId.TEMP_4, new UnsignedWordElement(1032))),
+				// Temp5-Temp8 not available
+
+				new FC3ReadRegistersTask(1037, Priority.LOW,
+						m(AcThor.ChannelId.H_W_2_MAX, new UnsignedWordElement(1037)),
+						m(AcThor.ChannelId.H_W_3_MAX, new UnsignedWordElement(1038)),
+						m(AcThor.ChannelId.H_W_2_MIN, new UnsignedWordElement(1039)),
+						m(AcThor.ChannelId.H_W_3_MIN, new UnsignedWordElement(1040)),
+						m(AcThor.ChannelId.RH_1_MAX, new UnsignedWordElement(1041)),
+						m(AcThor.ChannelId.RH_2_MAX, new UnsignedWordElement(1042)),
+						m(AcThor.ChannelId.RH_3_MAX, new UnsignedWordElement(1043)),
+						m(AcThor.ChannelId.RH_1_DAY_MIN, new UnsignedWordElement(1044)),
+						m(AcThor.ChannelId.RH_2_DAY_MIN, new UnsignedWordElement(1045)),
+						m(AcThor.ChannelId.RH_3_DAY_MIN, new UnsignedWordElement(1046)),
+						m(AcThor.ChannelId.RH_1_NIGHT_MIN, new UnsignedWordElement(1047)),
+						m(AcThor.ChannelId.RH_2_NIGHT_MIN, new UnsignedWordElement(1048)),
+						m(AcThor.ChannelId.RH_3_NIGHT_MIN, new UnsignedWordElement(1049)),
+						m(AcThor.ChannelId.NIGHT_FLAG, new UnsignedWordElement(1050))),
+
+				new FC3ReadRegistersTask(1057, Priority.HIGH,
+						m(AcThor.ChannelId.STRATIFACTION_FLAG, new UnsignedWordElement(1057)),
+						m(AcThor.ChannelId.RELAY_1_STATUS, new UnsignedWordElement(1058)),
+						new DummyRegisterElement(1059), // load state
+						m(AcThor.ChannelId.LOAD_NOMINAL_POWER, new UnsignedWordElement(1060)),
+						m(AsymmetricMeter.ChannelId.VOLTAGE_L1, new UnsignedWordElement(1061),
+								ElementToChannelConverter.SCALE_FACTOR_3),
+						m(AsymmetricMeter.ChannelId.CURRENT_L1, new UnsignedWordElement(1062),
+								ElementToChannelConverter.SCALE_FACTOR_2),
+						m(AcThor.ChannelId.VOLTAGE_OUT, new UnsignedWordElement(1063)),
+						m(SymmetricMeter.ChannelId.FREQUENCY, new UnsignedWordElement(1064)),
+						new DummyRegisterElement(1065, 1066), // Operation mode, Access level
+						m(AsymmetricMeter.ChannelId.VOLTAGE_L2, new UnsignedWordElement(1067),
+								ElementToChannelConverter.SCALE_FACTOR_3),
+						m(AsymmetricMeter.ChannelId.CURRENT_L2, new UnsignedWordElement(1068),
+								ElementToChannelConverter.SCALE_FACTOR_2),
+						m(AcThor.ChannelId.METER_POWER, new SignedWordElement(1069)), new DummyRegisterElement(1070), // Control
+																														// type
+						m(AcThor.ChannelId.PMAX_ABS, new UnsignedWordElement(1071)),
+						m(AsymmetricMeter.ChannelId.VOLTAGE_L3, new UnsignedWordElement(1072),
+								ElementToChannelConverter.SCALE_FACTOR_3),
+						m(AsymmetricMeter.ChannelId.CURRENT_L3, new UnsignedWordElement(1073),
+								ElementToChannelConverter.SCALE_FACTOR_2),
+						m(AcThor.ChannelId.P_OUT_1, new UnsignedWordElement(1074)),
+						m(AcThor.ChannelId.P_OUT_2, new UnsignedWordElement(1075)),
+						m(AcThor.ChannelId.P_OUT_3, new UnsignedWordElement(1076)),
+						m(AcThor.ChannelId.OPERATION_STATE, new UnsignedWordElement(1077)),
+						new DummyRegisterElement(1078, 1080),
+						m(AcThor.ChannelId.DEVICE_STATE, new UnsignedWordElement(1081)),
+						m(SymmetricMeter.ChannelId.ACTIVE_POWER, new UnsignedWordElement(1082)),
+						m(AcThor.ChannelId.SOLAR_PART_OF_DEVICE_POWER, new UnsignedWordElement(1083)),
+						m(AcThor.ChannelId.GRID_PART_OF_DEVICE_POWER, new UnsignedWordElement(1084))),
+
+				new FC6WriteRegisterTask(1000, m(AcThor.ChannelId.POWER, new UnsignedWordElement(1000))));
+	}
+
+	@Override
+	public MeterType getMeterType() {
+		return this.config.type();
+	}
+
+	@Override
+	public BooleanWriteChannel[] digitalOutputChannels() {
+		return this.outputChannels;
+	}
+
+	@Override
+	public String debugLog() {
+		return this.getActivePower().asString();
+	}
+
+	@Override
+	public Timedata getTimedata() {
+		return this.timedata;
+	}
+
+}
